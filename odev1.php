b1bc3f30d2a312a30ec1d31ee551c9653a18afe1
Taha Furkan <91225061+tahafurkanbayar@users.noreply.github.com>
2022-11-16 11:00:04 +0300

    Add files via upload

---
+++ odev1.php
@@ -0,0 +1,14 @@
+<?php
+// Evren ÇILGIN
+function ucgen($x){
+    for ($i=0;$i<$x;$i++){
+        $sayac=0;
+        while ($sayac<$i){
+            echo "O";
+            $sayac++;
+        }
+        echo "</br>";
+    }
+}
+ucgen(25);
+?>
